b32b843707c3d553563d178d33dcea222b38019b
jinyong <zhaqh146@naver.com>
2023-03-07 10:21:12 +0900

    feat : 튜토리얼 맵 개발 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
-import { useState } from "react";
-import MainPage from "./pages/MainPage";
-import EndPage from "./pages/EndPage";
+// import { useState } from "react";
+// import MainPage from "./pages/MainPage";
+// import EndPage from "./pages/EndPage";
+import Canvas from "./pages/Canvas";
 
 function App() {
-  const [isMain, setIsMain] = useState<boolean>(true);
+  // const [isMain, setIsMain] = useState<boolean>(true);
 
   return (
     <>
-      <button onClick={() => setIsMain(!isMain)}>{isMain ? "엔드페이지 보기" : "메인페이지 보기"}</button>
-      {isMain ? <MainPage /> : <EndPage /> }
+      {/* <button onClick={() => setIsMain(!isMain)}>{isMain ? "엔드페이지 보기" : "메인페이지 보기"}</button>
+      {isMain ? <MainPage /> : <EndPage/>} */}
+      <Canvas />
     </>
   );
 }
