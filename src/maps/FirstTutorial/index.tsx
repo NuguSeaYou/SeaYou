2c0b9911652a56b741753e6f7aca12b115be2a9e
J054_김준태 <kjt8709@nate.com>
2023-03-13 13:12:44 +0900

    feat : 바바 이동 구현 및 이동 취소 구현 #9

--- src/maps/FirstTutorial/index.tsx
+++ src/maps/FirstTutorial/index.tsx
@@ -12,104 +12,70 @@ const gameBoard = [
   [3, 3, 3, 3, 3, 3, 3, 3, 3],
 ];
 
+type contentType = {
+  id: number;
+  imgURL: string;
+  imgSize: number;
+};
+
+type positionType = {
+  x: number;
+  y: number;
+};
+
 export default function FirstTutorial() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [position, setPosition] = useState({ x: 2, y: 2 });
-  const [board, setBoard] = useState<number[][]>(gameBoard);
+  const [position, setPosition] = useState<positionType>({
+    x: tutorialMap1.userPosition[0],
+    y: tutorialMap1.userPosition[1],
+  });
+  const [board, setBoard] = useState<number[][]>(tutorialMap1.map);
   const [startGame, setStartGame] = useState(false);
-  const [prevKey, setPrevKey] = useState("");
-  const [history, setHistory] = useState<number[][]>([]);
+  const [onControl, setOnControl] = useState(false);
+  const [history, setHistory] = useState<string[]>([]);
 
   const startHandler = () => {
     setStartGame(true);
   };
 
-  // useEffect(() => {
-  //   const newBoard = [...board];
-  //   for (let i = 0; i < newBoard.length; i++) {
-  //     for (let j = 0; j < newBoard[i].length; j++) {
-  //       if (newBoard[i][j] === 1) {
-  //         newBoard[i][j] = 0;
-  //       }
-  //     }
-  //   }
-  //   newBoard[position.y][position.x] = 1;
-  //   setBoard(newBoard);
-  // }, [position]);
+  useEffect(() => {
+    const newBoard = [...board];
+    for (let i = 0; i < newBoard.length; i++) {
+      for (let j = 0; j < newBoard[i].length; j++) {
+        if (newBoard[i][j] === 1) {
+          newBoard[i][j] = 0;
+        }
+      }
+    }
+    newBoard[position.y][position.x] = 1;
+    setBoard(newBoard);
+  }, [position]);
 
   useEffect(() => {
-    drawBackground();
-    //drawCharacter();
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
+        drawInCanvas(tutorialMap1.contents.Background, j * 50, i * 50);
         if (board[i][j] === 1) {
-          drawCharacter();
-        }
-        if (board[i][j] === 2) {
-          drawRock(j * 100, i * 100);
-        }
-        if (board[i][j] === 3) {
-          drawBrick(j * 100, i * 100);
-        }
-        if (board[i][j] === 4) {
-          drawTarget(j * 100, i * 100);
+          drawInCanvas(tutorialMap1.contents.User, j * 50, i * 50);
+        } else if (board[i][j] === 2) {
+          drawInCanvas(tutorialMap1.contents.Rock, j * 50, i * 50);
+        } else if (board[i][j] === 3) {
+          drawInCanvas(tutorialMap1.contents.Brick, j * 50, i * 50);
+        } else if (board[i][j] === 4) {
+          drawInCanvas(tutorialMap1.contents.Flag, j * 50, i * 50);
         }
       }
     }
   }, [startGame, board]);
 
-  const drawBackground = () => {
+  const drawInCanvas = (content: contentType, x: number, y: number) => {
     drawImg({
       canvas: canvasRef,
-      newImage: "/bg.png",
-      x: 0,
-      y: 0,
-      w: board[0].length * 100 + 100,
-      h: board.length * 100 + 100,
-    });
-  };
-
-  const drawCharacter = () => {
-    drawImg({
-      canvas: canvasRef,
-      newImage: "/baba.png",
-      x: position.x * 100,
-      y: position.y * 100,
-      w: 100,
-      h: 100,
-    });
-  };
-
-  const drawBrick = (positionX: number, positionY: number) => {
-    drawImg({
-      canvas: canvasRef,
-      newImage: "/brick.png",
-      x: positionX,
-      y: positionY,
-      w: 100,
-      h: 100,
-    });
-  };
-
-  const drawRock = (positionX: number, positionY: number) => {
-    drawImg({
-      canvas: canvasRef,
-      newImage: "/circle.png",
-      x: positionX,
-      y: positionY,
-      w: 100,
-      h: 100,
-    });
-  };
-
-  const drawTarget = (positionX: number, positionY: number) => {
-    drawImg({
-      canvas: canvasRef,
-      newImage: "/target.png",
-      x: positionX,
-      y: positionY,
-      w: 100,
-      h: 100,
+      newImage: content.imgURL,
+      x,
+      y,
+      w: content.imgSize,
+      h: content.imgSize,
     });
   };
 
@@ -118,22 +84,18 @@ export default function FirstTutorial() {
   };
 
   const direction: directionType = {
-    ArrowUp: [-1, 0],
-    ArrowDown: [1, 0],
-    ArrowLeft: [0, -1],
-    ArrowRight: [0, 1],
+    ArrowUp: [0, -1],
+    ArrowDown: [0, 1],
+    ArrowLeft: [-1, 0],
+    ArrowRight: [1, 0],
   };
 
   const histroyHandler = (key: string) => {
-    const newHistory: number[][] = [...history];
-    const reverseDirection = [direction[key][0] * -1, direction[key][1] * -1];
-    if (direction[key]) {
-      setHistory([...newHistory, reverseDirection]);
-    }
+    const newHistory: string[] = [...history];
+    if (direction[key]) setHistory([...newHistory, key]);
   };
 
   const checkCanMove = (x: number, y: number, isCancle: boolean) => {
-    if (isCancle) return false;
     if (board[x][y] === 3) return false;
     if (board[x][y] === 2) return false;
     return true;
@@ -146,19 +108,32 @@ export default function FirstTutorial() {
     const check = checkCanMove(newPosition.x, newPosition.y, isCancle);
     if (!check) return;
     !isCancle && histroyHandler(key);
+    console.log(position, newPosition);
     setPosition({ ...newPosition });
   };
 
-  const characterHandler = (event: React.KeyboardEvent<HTMLCanvasElement>) => {
-    // if (prevKey === "Control" && (event.key === "z" || event.key === "Z")) {
-    //   if (history) {
-    //     const recentHistory = history.pop();
-    //     recentHistory && moveHandler(recentHistory, true);
-    //   }
-    // }
-    // setPrevKey(event.key);
+  const controlHandler = (event: React.KeyboardEvent<HTMLCanvasElement>) => {
+    if (event.key === "Control") {
+      setOnControl(false);
+    }
+  };
 
-    moveHandler(event.key);
+  const characterHandler = (event: React.KeyboardEvent<HTMLCanvasElement>) => {
+    if (onControl && (event.key === "z" || event.key === "Z")) {
+      if (history) {
+        const recentHistory = history.pop();
+        if ("ArrowUp" === recentHistory) moveHandler("ArrowDown", true);
+        if ("ArrowDown" === recentHistory) moveHandler("ArrowUp", true);
+        if ("ArrowLeft" === recentHistory) moveHandler("ArrowRight", true);
+        if ("ArrowRight" === recentHistory) moveHandler("ArrowLeft", true);
+      }
+    }
+    if (event.key === "Control") {
+      setOnControl(true);
+    }
+    if (direction[event.key]) {
+      moveHandler(event.key);
+    }
   };
 
   return (
@@ -170,9 +145,10 @@ export default function FirstTutorial() {
       />
       <canvas
         onKeyDown={characterHandler}
+        onKeyUp={controlHandler}
         ref={canvasRef}
-        width={board[0].length * 100}
-        height={board.length * 100}
+        width={board[0].length * 50}
+        height={board.length * 50}
         tabIndex={0}
       />
     </div>
